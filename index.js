d9df1ad36cd3b47fc58c8361b8e82cdd80050638
Ben Silverstein <silverstein.benjamin.a@gmail.com>
2019-08-22 13:16:03 -0400

    second push

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 let STORE = {
  lat: 38.889651,
- lng: -77.03525
+ lng: -77.03525,
 }
+
 
 
 function initMap() {
     var options = {  
-        zoom: 20, 
-        center: {lat: STORE.lat, lng: STORE.lng }}
-    let map = new google.maps.Map(document.getElementById('map'), (options)); 
+        zoom: 3, 
+        mapTypeControlOptions: false,
+        center: {lat: STORE.lat, lng: STORE.lng},
+        disableDefaultUI: true,
+        scaleControl: true,
+        zoomControl: true,
+        zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL
+        }}
+    map = new google.maps.Map(document.getElementById('map'), (options)); 
     }
 
-   $(initMap)
+    function autoSearch () {
+        let input = document.getElementByID('pac-input');
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var autocomplete = new google.maps.places.Autocomplete(input, options)
+    }
+
+function renderDoc () {
+    initMap()
+    autoSearch()
+}
+
+   $(renderDoc)
